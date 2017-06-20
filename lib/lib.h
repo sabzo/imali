853d073c72059ae384d22019b205ce6f0ff61a83
Sabelo <sabelo@sabelo.io>
2017-06-20 02:15:16 -0400

    changed input message to be 32 bytes

--- lib/lib.h
+++ lib/lib.h
@@ -2,21 +2,21 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define MESSAGE ((const unsigned char *) "test")
-#define MESSAGE_LEN 4
+#define MESSAGE_LEN 32
 
 void get_sha_256(unsigned char out[crypto_hash_sha256_BYTES]) {
-  unsigned char message[32]; // 32 bytes
+  unsigned char msg[32]; // 32 bytes
   int fd;
   int n_read;
 
   fd = open("/dev/urandom", O_RDONLY, 0);
-  // TODO: ensure message < (1.158 * 10^77) -1, which is slightly less than 2^256
-  if ((n_read = read(fd, message, 32)) == -1) { 
+  // TODO: To match Bitcoin elliptic curve key gen ensure msg bits are < (1.158 * 10^77) -1, which is slightly less than 2^256
+
+  if ((n_read = read(fd, msg, MESSAGE_LEN)) == -1) { 
     printf("Error getting random values\n");
     exit(1);
   }
 
-  crypto_hash_sha256(out, MESSAGE, MESSAGE_LEN);
+  crypto_hash_sha256(out, msg, MESSAGE_LEN);
   close(fd);
 }
